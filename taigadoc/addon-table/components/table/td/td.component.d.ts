a51aa1b5c03a7c85713c9c37f4b54a6c93a6d5af
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2025-01-14 08:11:13 +0100

    suppression de la doc taiga sur le workflow git

--- taigadoc/addon-table/components/table/td/td.component.d.ts
+++
@@ -1,6 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class TuiTableTd {
-    protected readonly control: unknown;
-    static ɵfac: i0.ɵɵFactoryDeclaration<TuiTableTd, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<TuiTableTd, "th[tuiTd], td[tuiTd]", never, {}, {}, ["control"], ["*"], true, never>;
-}
